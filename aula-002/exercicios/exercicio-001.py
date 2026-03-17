410e495f84810eeb5447178eb6ff43b0c55333a3
caio-fiap <rm572873@fiap.com.br>
2026-03-17 10:13:25 -0300

    aula 002 - exercicios

--- aula-002/exercicios/exercicio-001.py
+++ aula-002/exercicios/exercicio-001.py
@@ -1,1 +1,9 @@
 print("=" * 10, "Exercício 1", "=" * 10)
+
+print("\nCálculo de área de círculo\n")
+
+raio = float(input("Indique o raio do círculo: "))
+
+area = 3.14159 * (raio ** 2)
+
+print(f"\nA área do círculo é, aproximadamente, {area:.2f} u² (unidades quadradas)")
---
+++ aula-002/exercicios/exercicio-002.py
@@ -0,0 +1,9 @@
+print("=" * 10, "Exercício 2", "=" * 10)
+
+print("\nConversor Celcius-Fahrenheit\n")
+
+F = float(input("Temperatura em Fahrenheit: "))
+
+C = (F - 32) * (5 / 9)
+
+print(f"A temperatura em Celcius é: {C:.1f} °")
