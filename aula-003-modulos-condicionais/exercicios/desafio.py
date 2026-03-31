cfe78b48b0199ebb2a88a4c6476ae8335c8a7ea0
caio-fiap <rm572873@fiap.com.br>
2026-03-31 09:45:59 -0300

    update desafio.py

--- aula-003-modulos-condicionais/exercicios/desafio.py
+++ aula-003-modulos-condicionais/exercicios/desafio.py
@@ -1,1 +1,10 @@
 # receber idade e indicar se o voto é opicional
+
+idade = int(input("Digite sua idade: "))
+
+if idade < 16:
+    print("Você não pode votar")
+elif 70 > idade >= 18:
+    print("Você é obrigado a votar")
+else:
+    print("Você não é obrigado a votar")
