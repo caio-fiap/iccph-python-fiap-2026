9ef4d1ca58ef66132efffb08d526d1adf016fe58
caio-fiap <rm572873@fiap.com.br>
2026-03-31 08:27:27 -0300

    organização do repositorio

--- aula-002/variaveis.py
+++ aula-002/variaveis.py
@@ -3,6 +3,7 @@
 print("ola mundo")
 print(9 + 1)
 print('9'+'1')
+print("9 + 1")
 
 # variaveis
 
@@ -14,9 +15,9 @@ print(nome, idade, peso)
 
 # input - simula forms no cmd
 
-nome = input("Digite seu nome: ")
-idade = int(input("Digite sua idade: "))
-peso = float(input("Digite seu peso: "))
+nome = input("Digite seu nome: ") # str
+idade = int(input("Digite sua idade: ")) # int
+peso = float(input("Digite seu peso: ")) # float
 
 print(f"Oiii, {nome}!!!")
 
