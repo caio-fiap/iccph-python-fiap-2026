b27e7a001e61bdcc23cad8b88486a2229d08dbb7
caio-fiap <rm572873@fiap.com.br>
2026-03-31 09:00:37 -0300

    modulos

---
+++ aula-003-modulos-condicionais/modulos.py
@@ -0,0 +1,20 @@
+import math
+
+num = 17
+raiz = math.sqrt(num)
+
+print(f"a raiz quadrada de {num} é {raiz:.2f}")
+
+graus = 45
+radiano = graus / 180 * math.pi
+seno = math.sin(radiano)
+print(seno)
+print()
+
+import random
+
+num_rand = random.random()
+print(num_rand)
+
+num_rand_int = random.randint(1, 10)
+print(num_rand_int)
