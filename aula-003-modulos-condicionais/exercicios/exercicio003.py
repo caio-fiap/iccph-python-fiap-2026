379303279d462d14a99b8674ca41b129df20a42b
caio-fiap <rm572873@fiap.com.br>
2026-04-07 09:00:35 -0300

    ex003

---
+++ aula-003-modulos-condicionais/exercicios/exercicio003.py
@@ -0,0 +1,9 @@
+num_a = float(input("Informe um número: "))
+num_b = float(input("Informe outro número: "))
+
+if num_a > num_b:
+    print(f"Maior: {num_a}")
+elif num_b > num_a:
+    print(f"Maior: {num_b}")
+else:
+    print("Os números são iguais")
