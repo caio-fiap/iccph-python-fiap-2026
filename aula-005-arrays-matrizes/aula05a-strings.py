0573ae580d740388dba4c14157859f5ed82246fa
caio-fiap <rm572873@fiap.com.br>
2026-05-05 08:30:26 -0300

    aula05a-strings

---
+++ aula-005-arrays-matrizes/aula05a-strings.py
@@ -0,0 +1,17 @@
+texto = "hello world"
+
+print(texto[0])
+print(texto[1])
+print(texto[2])
+print(texto[3])
+print()
+
+tamanho = len(texto)
+print(tamanho)
+print()
+
+#for i in range(len(texto)):
+#    print(f"texto[{i}] = {texto[i]}")
+
+for c in texto:
+    print(c)
