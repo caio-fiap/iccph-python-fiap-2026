9ef56c8d52102f10d103a1ae01cef3c36af82eab
caio-fiap <rm572873@fiap.com.br>
2026-05-05 09:36:40 -0300

    aula05c-matrizes

---
+++ aula-005-arrays-matrizes/aula05c-matriz.py
@@ -0,0 +1,10 @@
+musicas = [
+    ["Roda-viva", "Chico Buarque"],
+    ["Dumb", "Nirvana"],
+    ["Symphony No. 5", "Beethoven"]
+]
+
+for musica in musicas:
+    for info in musica:
+        print(info)
+    print()
